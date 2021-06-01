1eb4ae3eba53f50e97bee6af80995352214702a4
vivekjain3637 <vivekjain3637@gmail.com>
2021-06-01 22:45:45 +0530

    Initial Project

---
+++ src/main/java/com/springlearning/sfgpetclinic/SfgPetClinicSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicSpringBootApplication.class, args);
+    }
+
+}
